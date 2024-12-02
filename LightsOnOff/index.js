a48011db02360f8ef4b2d46c3ab1aba4ae047d39
jan07-ammu <januraman0719@gmail.com>
2024-12-02 22:57:30 +0530

    js session

---
+++ LightsOnOff/index.js
@@ -0,0 +1,55 @@
+// console.log("hello world");
+// console.log('hello world');
+// console.error('hello world');
+// console.table("hello world");
+// // console.warn("hello world");
+
+//java int double string var
+//java script let const var
+
+// let a={
+//     Name:"janaki",
+//     Nick_Name:"john",
+// }
+// let a=()=>{
+//     return "hello world!";
+// }
+
+
+// if(1<2)
+// {
+//     console.log("2 is greater");
+// }
+// else
+// {
+//     console.log("1 is greater");
+// }
+
+
+
+// for(let a=0;a<=10;a++)
+// {
+//     console.log(a);
+// }
+// console.log(a);
+
+
+// const b=10
+// console.log(b)
+
+
+let a1=10;
+let b1=20;
+// function sum(a1,b1)
+// {
+//             // console.log("Sum ="+(a1+b1));
+//             console.log("Sum of "+a1+" and "+b1+" is "+(a1+b1));
+// }
+// sum(a1,b1);
+
+
+
+const sum1=(a1,b1)=>{
+    console.log("Sum of "+a1+" and "+b1+" is "+(a1+b1));
+}
+sum(a1,b1);
